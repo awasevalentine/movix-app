7e112f5d150634226ecb4aa89f37475438453a5c
awasevalentine <awasevalentine@gmail.com>
2023-01-13 10:41:50 +0100

    Added firebase configurations to the project

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+    apiKey: process.env.REACT_API_KEY,
+    authDomain: process.env.REACT_AUTH_DOMAIN,
+    projectId: process.env.REACT_PROJECT_ID,
+    storageBucket: process.env.REACT_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_MESSAGEING_SENDER_ID,
+    appId: process.env.REACT_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
